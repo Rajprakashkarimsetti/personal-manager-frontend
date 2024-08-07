41995b473d1b82ab9f3acaf96c33e74d9ee87f1d
Rajprakashkarimsetti <rajprakash.karimsetti@gmail.com>
2024-08-07 12:51:06 +0530

    fixed the error in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Correctly import Router
 import { HashRouter } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
@@ -9,7 +9,7 @@ import TransactionForm from './components/Dashboard/TransactionForm'; // Import 
 import EditTransactionPage from './pages/EditTransaction'; // Import EditTransactionPage
 
 const App = () => (
-  <HashRouter>
+  <HashRouter> {/* Use either Router or HashRouter */}
     <Routes>
       <Route path="/" element={<Login />} /> {/* Default route */}
       <Route path="/login" element={<Login />} />
@@ -17,7 +17,6 @@ const App = () => (
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/transaction" element={<TransactionForm />} /> {/* Route for TransactionPage */}
       <Route path="/edit-transaction/:id" element={<EditTransactionPage />} /> {/* Route for EditTransactionPage */}
-      {/* Remove the NotFound route if not needed */}
     </Routes>
   </HashRouter>
 );
